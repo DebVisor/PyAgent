782daea22ac2651f8c53efcb99269efab7005f5d
UndiFineD <keimpe@example.com>
2026-03-03 06:55:31 +0000

    auto fix basic syntactic cleanups

--- src/core/base/common/models/fleet_models.py
+++ src/core/base/common/models/fleet_models.py
@@ -23,7 +23,7 @@ from typing import Any
 from .base_models import (_empty_dict_str_any, _empty_dict_str_float,
                           _empty_dict_str_int, _empty_dict_str_str,
                           _empty_list_str)
-                          from .core_enums import RateLimitStrategy
+from .core_enums import RateLimitStrategy
 
 
 @dataclass(slots=True)
