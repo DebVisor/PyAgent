b1255ebdf07fb2d955a3f24fd63c60d8499e9b0a
UndiFineD <keimpe@example.com>
2026-03-03 06:53:54 +0000

    auto fix basic syntactic cleanups

--- src/logic/agents/intelligence/sql_query_agent.py
+++ src/logic/agents/intelligence/sql_query_agent.py
@@ -19,7 +19,7 @@ Sql query agent.py module.
 try:
     from .data_intelligence_agent import DataIntelligenceAgent
 except Exception:
-    from src.logic.agents.intelligence.data_intelligence_agent import DataIntelligenceAgent
+from src.logic.agents.intelligence.data_intelligence_agent import DataIntelligenceAgent
 
 
 class SqlQueryAgent(DataIntelligenceAgent):  # pylint: disable=too-many-ancestors
