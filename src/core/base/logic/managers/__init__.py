dbd2ac335ab1c4da4d38e8aacc4ffb61b8bb4a45
UndiFineD <keimpe@example.com>
2026-03-03 06:55:29 +0000

    auto fix basic syntactic cleanups

--- src/core/base/logic/managers/__init__.py
+++ src/core/base/logic/managers/__init__.py
@@ -26,13 +26,13 @@ from .batch_managers import BatchRequest, RequestBatcher  # noqa: F401
 from .conversation_managers import ConversationHistory  # noqa: F401
 from .orchestration_managers import (ABTest, AgentComposer, ModelSelector,  # noqa: F401
                                      QualityScorer)
-                                     from .plugin_manager import PluginManager, PluginMetadata  # noqa: F401
-                                     from .processor_managers import (MultimodalProcessor, ResponsePostProcessor,  # noqa: F401
+from .plugin_manager import PluginManager, PluginMetadata  # noqa: F401
+from .processor_managers import (MultimodalProcessor, ResponsePostProcessor,  # noqa: F401
                                  SerializationManager)
-                                 from .prompt_managers import (PromptTemplateManager, PromptVersion,  # noqa: F401
+from .prompt_managers import (PromptTemplateManager, PromptVersion,  # noqa: F401
                               PromptVersionManager)
-                              from .resource_quota_manager import QuotaConfig, ResourceQuotaManager  # noqa: F401
-                              from .system_managers import (EventManager, FilePriorityManager, HealthChecker,  # noqa: F401
+from .resource_quota_manager import QuotaConfig, ResourceQuotaManager  # noqa: F401
+from .system_managers import (EventManager, FilePriorityManager, HealthChecker,  # noqa: F401
                               ProfileManager, ResponseCache, StatePersistence)
 
 __version__ = VERSION
